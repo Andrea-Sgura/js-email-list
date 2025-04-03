8aa568c61cfd75672bed4d416f1dc443515f2d6d
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-03 17:27:15 +0200

    [UPD] Creata lista mail da mostrare

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const emailList = document.getElementById("email-list");
 for (let i=0; i<10; i++) {
     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
         console.log(resp.data.response); // ESEGUO UN CONTROLLO IN CONSOLE
+
+        // CREO IL NUOVO ELEMENTO 
+        const emailItem = document.createElement("ul");
+        emailItem.textContent = resp.data.response;
+        emailList.appendChild(emailItem);
     });
 }
 
