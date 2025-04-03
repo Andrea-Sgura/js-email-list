13568299239b0c057e82770fb6c9939a76cd9987
Andrea-Sgura <andreasgura96@gmail.com>
2025-04-03 17:19:47 +0200

    [UPD] Inserito un ciclo for per generare le 10 mail ed effettuato un controllo in console

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
-    console.log(resp.data.response);
-});
+// RECUPERO L'ELEMENTO DEL DOM DOVE VADO A MOSTRARE LE MAIL GENERATE
+const emailList = document.getElementById("email-list");
+
+// GENERO 10 MAIL INIZIALI INSERENDO UN CICLO FOR
+for (let i=0; i<10; i++) {
+    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+        console.log(resp.data.response); // ESEGUO UN CONTROLLO IN CONSOLE
+    });
+}
+
